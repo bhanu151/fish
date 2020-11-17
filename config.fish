ae262007c55a9dade7ffa2938ec4bf4bb1b599c7
Bhanu <bhanu151@users.noreply.github.com>
2020-11-17 15:10:57 +0530

    Adding fish config

---
+++ config.fish
@@ -0,0 +1,8 @@
+theme_gruvbox dark hard 
+set -g fish_key_bindings fish_vi_key_bindings
+
+#==============================================================================
+# KEY BINDINGS
+#==============================================================================
+bind -M normal \cf accept-autosuggestion
+bind -M insert \cf accept-autosuggestion
