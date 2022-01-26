0634fe147c80273f7cfbef321c3d2a1a78c8ba09
Bhanu Priya <bhanupriya151@gmail.com>
2022-01-26 18:57:43 +0530

    updated config

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-theme_gruvbox dark hard 
+#theme_gruvbox dark hard 
 set -g fish_key_bindings fish_vi_key_bindings
 
 #==============================================================================
@@ -6,3 +6,26 @@ set -g fish_key_bindings fish_vi_key_bindings
 #==============================================================================
 bind -M normal \cf accept-autosuggestion
 bind -M insert \cf accept-autosuggestion
+
+#==============================================================================
+# ALIASES
+#==============================================================================
+alias v="nvim"
+alias p="python3"
+alias gad="git add"
+alias gct="git commit"
+alias gph="git push"
+alias gpl="git pull"
+alias gss="git status"
+alias gdf="git diff"
+alias grm="git rm"
+alias glg="git log"
+
+
+#==============================================================================
+# GLOBAL ENVIRONMENT VARIABLES
+#==============================================================================
+#fish_add_path ~/.local/bin/
+#fish_add_path ~/.cargo/bin/
+set PATH ~/.local/bin ~/.cargo/bin $PATH
+
